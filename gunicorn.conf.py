fa5014528b69fbb7b8b5f241499faf06e8e94fb7
NathanRoland <nathanroland635@gmail.com>
2025-08-07 14:27:22 +1000

    gunicorn

---
+++ gunicorn.conf.py
@@ -0,0 +1,37 @@
+# Gunicorn configuration file
+import os
+
+# Server socket
+bind = "0.0.0.0:{}".format(os.environ.get("PORT", "5001"))
+backlog = 2048
+
+# Worker processes
+workers = 2
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Restart workers after this many requests, to help prevent memory leaks
+max_requests = 1000
+max_requests_jitter = 50
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# Process naming
+proc_name = "song-application"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (not needed on Render as they handle SSL)
+keyfile = None
+certfile = None 
